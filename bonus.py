d3b21b260e1a394139d597eac186bb1ce163cf4c
Artashes Yeritsyan <aryerits@c2r13s4.42yerevan.am>
2023-02-23 14:37:21 +0400

    Exceptions added

--- bonus.py
+++ bonus.py
@@ -1,28 +1,31 @@
 import time
 
-inputTime = input("Insert time to count down (h:m:s) ")
-sep = inputTime.split(":")
-h = int(sep[0])
-m = int(sep[1])
-s = int(sep[2])
-while h >= 0:
-    while m >= 0:
-        while s >= 0:
-            if h <= 9: 
-                print("0", h, ":", sep = "", end = "")
-            else:
-                print(h, ":", sep = "", end = "")
-            if m <= 9:
-                print("0", m, ":", sep = "", end = "")
-            else:
-                print(m, ":", sep = "", end = "")
-            if s <= 9:
-                print("0", s, sep = "")
-            else:
-                print(s, sep = "")            
-            time.sleep(1)
-            s -= 1
-        m -= 1
-        s = 59
-    h -= 1
-    m = 59
+try:
+    inputTime = input("Insert time to count down (h:m:s) ")
+    sep = inputTime.split(":")
+    h = int(sep[0])
+    m = int(sep[1])
+    s = int(sep[2])
+    while h >= 0:
+        while m >= 0:
+            while s >= 0:
+                if h <= 9:
+                    print("0", h, ":", sep = "", end = "")
+                else:
+                    print(h, ":", sep = "", end = "")
+                if m <= 9:
+                    print("0", m, ":", sep = "", end = "")
+                else:
+                    print(m, ":", sep = "", end = "")
+                if s <= 9:
+                    print("0", s, sep = "")
+                else:
+                    print(s, sep = "")
+                time.sleep(1)
+                s -= 1
+            m -= 1
+            s = 59
+        h -= 1
+        m = 59
+except:
+    print ("Please enter numbers and follow the form (h:m:s)")
